ff3c44cb413deab626a06d6d24b027981d4f49bc
Sean Jackson <seanjackson@Seans-MacBook-Pro.local>
2020-03-07 14:03:37 -0800

    final touches for submission to graders

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -10,7 +10,7 @@ class Home extends Component {
             <div>
                 <main role="main">
                     <header role="banner">
-                        <h1>How to Stop Smiling.<br/>A blog.</h1>
+                        <h1>How to Stop Smiling.<br/>A blog.</h1><br/><h6>for the purposes of demo, you can log in as a user with writer access using username: irene password: writer</h6>
                     </header>
                     <div className="Writer-Menu">
                         {this.context.users.map((user, key) => {
